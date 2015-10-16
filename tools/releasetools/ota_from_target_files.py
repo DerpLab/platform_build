e9756730ae3b243c58c97fcde513df15b19058a7
beanstown106 <nbenis106@gmail.com>
2015-10-16 13:03:27 +0100

    Clean up ROM flash and add DerpFest info
    * Drop the target build fingerprint text.
    * Drop the patching system image unconditionally text.
    * Drop the verifying system image text.
    * Changed the verified system image text.
    * Add flashing system text.
    
    Modified from original commit for DerpFest.
    
    Change-Id: Icc2197b000dbaa70187a8e247a9faf28dbf59c4d
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -1016,6 +1016,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.ShowProgress(0.02, 10)
     script.RunBackup("restore", sysmount)
 
+  script.Print(" ")
+  script.Print("Flashing boot image..")
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
