591ceba4597f11ff2b5e388463f7c9848197df71
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-06 21:16:47 -0300

    build: Remove some apps by default
    Change-Id: Ic1478ccfbb88f8cae49f18d51540a85d24a6f9ac
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -32,9 +32,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 endif
 
-PRODUCT_PACKAGES += \
-    WallpaperPicker \
-
 ifeq ($(AOSIP_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif
